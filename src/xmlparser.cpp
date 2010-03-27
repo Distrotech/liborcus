6e0d313bc8e205754862c35c8b0e181bd9b04bc6
Kohei Yoshida <kyoshida@novell.com>
2010-03-27 12:26:57 -0400

    Initial skeleton code for implementing libxml's sax interface.

--- src/xmlparser.cpp
+++ src/xmlparser.cpp
@@ -36,6 +36,90 @@ using namespace std;
 
 namespace orcus {
 
+namespace {
+
+struct user_data
+{
+};
+
+void start_element(void* ctx, const xmlChar* name, const xmlChar** attrs)
+{
+    cout << "start element: " << name << endl;
+}
+
+void end_element(void* ctx, const xmlChar* name)
+{
+    cout << "end element: " << name << endl;
+}
+
+void start_element_ns(
+    void *ctx,
+    const xmlChar *localname,
+    const xmlChar *prefix,
+    const xmlChar *URI,
+    int nb_namespaces,
+    const xmlChar **namespaces,
+    int nb_attributes,
+    int nb_defaulted,
+    const xmlChar **attributes)
+{
+}
+
+void end_element_ns(
+    void *ctx,
+    const xmlChar *localname,
+    const xmlChar *prefix,
+    const xmlChar *URI)
+{
+}
+
+void characters(void* ctx, const xmlChar* ch, int len)
+{
+    cout << "chars: ";
+    for (int i = 0; i < len; ++i)
+        cout << ch[i];
+    cout << endl;
+}
+
+xmlSAXHandler sax_handler_struct = {
+    NULL, /* internalSubset */
+    NULL, /* isStandalone */
+    NULL, /* hasInternalSubset */
+    NULL, /* hasExternalSubset */
+    NULL, /* resolveEntity */
+    NULL, /* getEntity */
+    NULL, /* entityDecl */
+    NULL, /* notationDecl */
+    NULL, /* attributeDecl */
+    NULL, /* elementDecl */
+    NULL, /* unparsedEntityDecl */
+    NULL, /* setDocumentLocator */
+    NULL, /* startDocument */
+    NULL, /* endDocument */
+    start_element,
+    end_element,
+    NULL, /* reference */
+    characters,
+    NULL, /* ignorableWhitespace */
+    NULL, /* processingInstruction */
+    NULL, /* comment */
+    NULL, /* xmlParserWarning */
+    NULL, /* xmlParserError */
+    NULL, /* xmlParserError */
+    NULL, /* getParameterEntity */
+    NULL, /* cdataBlock; */
+    NULL, /* externalSubset; */
+    1,
+    NULL,
+    start_element_ns,
+    end_element_ns,
+    NULL  /* xmlStructuredErrorFunc */
+};
+
+static xmlSAXHandlerPtr sax_handler = &sax_handler_struct;
+
+}
+
 xml_stream_parser::xml_stream_parser(const uint8_t* content, size_t size, const string& name) :
     m_content(content), m_size(size), m_name(name)
 {
@@ -47,16 +131,8 @@ xml_stream_parser::~xml_stream_parser()
 
 void xml_stream_parser::parse()
 {
-    xmlDocPtr doc = xmlReadMemory(
-        reinterpret_cast<const char*>(m_content), m_size, "test", NULL, 0);
-
-    if (!doc)
-        return;
-
-    cout << "doc = " << doc << endl;
-    xmlDocDump(stdout, doc);
-    xmlFreeDoc(doc);
-    xmlCleanupParser();
+    user_data data;
+    xmlSAXUserParseMemory(sax_handler, &data, reinterpret_cast<const char*>(m_content), m_size);
 }
 
 }
