b2ccd2aa9702e8a09aa5a3642e394f118db1bda0
Kohei Yoshida <kyoshida@novell.com>
2010-12-11 16:34:55 -0500

    More on parsing [Content_Types].xml part.

--- src/ooxml/opc_context.cpp
+++ src/ooxml/opc_context.cpp
@@ -26,8 +26,45 @@
  ************************************************************************/
 
 #include "ooxml/opc_context.hpp"
+#include "ooxml/opc_token_constants.hpp"
+#include "global.hpp"
+
+#include <algorithm>
+
+using namespace std;
 
 namespace orcus {
+
+namespace {
+
+class types_attr_parser : public unary_function<void, xml_attr_t>
+{
+public:
+    explicit types_attr_parser() : 
+        m_default_ns(XMLNS_UNKNOWN_TOKEN) {}
+
+    types_attr_parser(const types_attr_parser& r) :
+        m_default_ns(r.m_default_ns) {}
+
+    void operator() (const xml_attr_t& attr)
+    {
+        if (attr.ns == XMLNS_UNKNOWN_TOKEN && attr.name == XML_xmlns)
+        {
+            if (attr.value != "http://schemas.openxmlformats.org/package/2006/content-types")
+                throw xml_structure_error("invalid namespace for types element!");
+            m_default_ns = XMLNS_ct;
+        }
+    }
+
+    xmlns_token_t get_default_ns() const
+    {
+        return m_default_ns;
+    }
+private:
+    xmlns_token_t m_default_ns;
+};
+
+}
 
 opc_content_types_context::opc_content_types_context(const tokens& _tokens) :
     xml_context_base(_tokens)
@@ -51,7 +88,31 @@ void opc_content_types_context::end_child_context(xmlns_token_t ns, xml_token_t 
 void opc_content_types_context::start_element(xmlns_token_t ns, xml_token_t name, const::std::vector<xml_attr_t> &attrs)
 {
     xml_token_pair_t parent = push_stack(ns, name);
-    warn_unhandled();
+    switch (name)
+    {
+        case XML_Types:
+        {
+            xml_element_expected(parent, XMLNS_UNKNOWN_TOKEN, XML_UNKNOWN_TOKEN);
+
+            print_attrs(get_tokens(), attrs);
+
+            xmlns_token_t default_ns = 
+                for_each(attrs.begin(), attrs.end(), types_attr_parser()).get_default_ns();
+
+            // the namespace for worksheet element comes from its own 'xmlns' attribute.
+            get_current_element().first = default_ns;
+            set_default_ns(default_ns);
+        }
+        break;
+        case XML_Override:
+            xml_element_expected(parent, XMLNS_ct, XML_Types);
+        break;
+        case XML_Default:
+            xml_element_expected(parent, XMLNS_ct, XML_Types);
+        break;
+        default:
+            warn_unhandled();
+    }
 }
 
 bool opc_content_types_context::end_element(xmlns_token_t ns, xml_token_t name)
