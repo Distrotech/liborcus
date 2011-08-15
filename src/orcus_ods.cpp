65a612c676d0e4550d7e291c3c2376b9dad8a841
Kohei Yoshida <kyoshida@novell.com>
2011-08-14 23:42:40 -0400

    Removed calls to libgsf from orcus-ods.

--- src/orcus_ods.cpp
+++ src/orcus_ods.cpp
@@ -25,10 +25,7 @@
  *
  ************************************************************************/
 
-#include <gsf/gsf-utils.h>
-#include <gsf/gsf-input-stdio.h>
-#include <gsf/gsf-infile.h>
-#include <gsf/gsf-infile-zip.h>
+#include <zip.h>
 
 #include "orcus/xml_parser.hpp"
 #include "orcus/odf/ods_handler.hpp"
@@ -57,9 +54,9 @@ public:
     void read_file(const char* fpath, const char* outpath);
 
 private:
-    void list_content (GsfInput* input, int level = 0);
-    void read_content(GsfInput* input, const char* outpath);
-    void read_content_xml(GsfInput* input, size_t size, const char* outpath);
+    void list_content(struct zip* archive) const;
+    void read_content(struct zip* archive);
+    void read_content_xml(const char* p, size_t size);
 
 private:
     ::boost::shared_ptr<model::factory_base> mp_factory;
@@ -74,89 +71,66 @@ orcus_ods::~orcus_ods()
 {
 }
 
-void orcus_ods::read_content_xml(GsfInput* input, size_t size, const char* outpath)
+void orcus_ods::list_content(struct zip* archive) const
 {
-    const guint8* content = gsf_input_read(input, size, NULL);
-    xml_stream_parser parser(odf_tokens, content, size, "content.xml");
+    zip_uint64_t num = zip_get_num_entries(archive, 0);
+    cout << "number of files this archive contains: " << num << endl;
+
+    for (zip_uint64_t i = 0; i < num; ++i)
+    {
+        const char* filename = zip_get_name(archive, i, 0);
+        cout << filename << endl;
+    }
+}
+
+void orcus_ods::read_content(struct zip* archive)
+{
+    if (!archive)
+        return;
+
+    struct zip_stat file_stat;
+    if (zip_stat(archive, "content.xml", 0, &file_stat))
+    {
+        cout << "failed to get stat on content.xml" << endl;
+        return;
+    }
+
+    cout << "name: " << file_stat.name << "  size: " << file_stat.size << endl;
+    struct zip_file* zfd = zip_fopen(archive, file_stat.name, 0);
+    if (zfd)
+    {
+        string buf(static_cast<size_t>(file_stat.size), 0);
+        int buf_read = zip_fread(zfd, &buf[0], file_stat.size);
+        cout << "actual buffer read: " << buf_read << endl;
+        if (buf_read > 0)
+        read_content_xml(&buf[0], buf_read);
+        zip_fclose(zfd);
+    }
+}
+
+void orcus_ods::read_content_xml(const char* p, size_t size)
+{
+    xml_stream_parser parser(odf_tokens, reinterpret_cast<const uint8_t*>(p), size, "content.xml");
     ::boost::scoped_ptr<ods_content_xml_handler> handler(
         new ods_content_xml_handler(odf_tokens, mp_factory.get()));
     parser.set_handler(handler.get());
     parser.parse();
-//  handler->print_html(outpath);
-}
-
-void orcus_ods::read_content(GsfInput* input, const char* outpath)
-{
-    if (!GSF_IS_INFILE(input))
-        return;
-
-    GsfInput* content_xml = gsf_infile_child_by_name (GSF_INFILE (input), "content.xml");
-    if (content_xml)
-    {
-        const char* name = gsf_input_name(content_xml);
-        size_t size = gsf_input_size(content_xml);
-        cout << "name: " << name << "  size: " << size << endl;
-        read_content_xml(content_xml, size, outpath);
-        g_object_unref(G_OBJECT(content_xml));
-    }
-}
-
-void orcus_ods::list_content (GsfInput* input, int level)
-{
-    if (!GSF_IS_INFILE(input))
-        return;
-
-    int child_count = gsf_infile_num_children (GSF_INFILE (input));
-    bool is_dir = child_count >= 0;
-    
-    for (int i = 0; i < level; ++i)
-        printf("   ");
-
-    if (!is_dir)
-        return;
-
-    for (int i = 0; i < level; ++i)
-        printf("   ");
-
-    puts ("{");
-    for (int i = 0 ; i < child_count; ++i)
-    {    
-        GsfInput* child = gsf_infile_child_by_index (GSF_INFILE (input), i);
-        list_content(child, level+1);
-        g_object_unref(G_OBJECT(child));
-    }
-
-    for (int i = 0; i < level; ++i)
-        printf("   ");
-    puts ("}");
 }
 
 void orcus_ods::read_file(const char* fpath, const char* outpath)
 {
     cout << "reading " << fpath << endl;
-
-    gsf_init();
-
-    GError* err = NULL;
-    GsfInput* input = gsf_input_stdio_new (fpath, &err);
-    if (!input)
-    {    
-        g_error_free (err);
-        return;
-    }
-
-    GsfInfile* infile = gsf_infile_zip_new (input, &err);
-    if (!infile)
+    int error;
+    struct zip* archive = zip_open(fpath, 0, &error);
+    if (!archive)
     {
-        g_error_free (err);
+        cout << "failed to open " << fpath << endl;
         return;
     }
 
-    read_content (GSF_INPUT(infile), outpath);
-    g_object_unref (G_OBJECT (infile));
-    g_object_unref (G_OBJECT (input));
-
-    gsf_shutdown();
+    list_content(archive);
+    read_content(archive);
+    zip_close(archive);
 }
 
 int main(int argc, char** argv)
