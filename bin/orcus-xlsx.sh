755736b5ac18dd6fdb1a311b13620f9585238036
Kohei Yoshida <kohei.yoshida@gmail.com>
2012-08-24 13:15:07 -0400

    Fix orcus-xlsx.sh and orcus-ods.sh scripts.

--- bin/orcus-xlsx.sh
+++ bin/orcus-xlsx.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-EXEC=orcus-xlsx
 PROGDIR=`dirname $0`
-EXECPATH=$PROGDIR/../src/.libs/$EXEC
-export LD_LIBRARY_PATH=$PROGDIR/../src/liborcus/.libs
-$EXECPATH "$@"
+source $PROGDIR/orcus-common-func.sh
+orcus_exec orcus-xlsx "$PWD" "$@"
