662e68941d138d55257ea7f2671eab674da0e0e4
Kohei Yoshida <kohei.yoshida@gmail.com>
2012-10-04 22:36:55 -0400

    Test the xml export part of orcus-xml.
    For the purpose of this test, the output xml should be structurally
    identical to the input xml.

--- src/orcus_test_xml_mapped.cpp
+++ src/orcus_test_xml_mapped.cpp
@@ -27,10 +27,13 @@
 
 #include "orcus/orcus_xml.hpp"
 #include "orcus/global.hpp"
+#include "orcus/sax_parser.hpp"
+
 #include "spreadsheet/factory.hpp"
 #include "spreadsheet/document.hpp"
 
 #include "xml_map_sax_handler.hpp"
+#include "dom_tree_sax_handler.hpp"
 
 #include <cstdlib>
 #include <cassert>
@@ -40,6 +43,8 @@
 
 #include <boost/scoped_ptr.hpp>
 
+#include <unistd.h>
+
 using namespace std;
 using namespace orcus;
 
@@ -47,6 +52,19 @@ const char* files[] = {
     "../test/xml-mapped/content-basic",
     "../test/xml-mapped/attribute-basic"
 };
+
+const char* temp_output_xml = "out.xml";
+
+void dump_xml_structure(string& dump_content, const char* filepath)
+{
+    string strm;
+    load_file_content(filepath, strm);
+    dom_tree_sax_handler hdl;
+    sax_parser<dom_tree_sax_handler> parser(strm.c_str(), strm.size(), hdl);
+    ostringstream os;
+    hdl.dump_compact(os);
+    dump_content = os.str();
+}
 
 void test_mapped_xml_import()
 {
@@ -64,10 +82,11 @@ void test_mapped_xml_import()
         load_file_content(data_file.c_str(), strm);
 
         boost::scoped_ptr<spreadsheet::document> doc(new spreadsheet::document);
-        boost::scoped_ptr<spreadsheet::import_factory> fact(new spreadsheet::import_factory(doc.get()));
+        boost::scoped_ptr<spreadsheet::import_factory> import_fact(new spreadsheet::import_factory(doc.get()));
+        boost::scoped_ptr<spreadsheet::export_factory> export_fact(new spreadsheet::export_factory(doc.get()));
 
         // Parse the map file to define map rules, and parse the data file.
-        orcus_xml app(fact.get(), NULL);
+        orcus_xml app(import_fact.get(), export_fact.get());
         read_map_file(app, map_file.c_str());
         app.read_file(data_file.c_str());
 
@@ -85,6 +104,22 @@ void test_mapped_xml_import()
         p1 = p1.trim();
         p2 = p2.trim();
         assert(p1 == p2);
+
+        // Output to xml file with the linked values coming from the document.
+        string out_file = base_dir + "/" + temp_output_xml;
+        cout << "writing to " << out_file << endl;
+        app.write_file(out_file.c_str());
+
+        // Compare the logical xml content of the output xml with the input
+        // one. They should be identical.
+
+        string dump_input, dump_output;
+        dump_xml_structure(dump_input, data_file.c_str());
+        dump_xml_structure(dump_output, out_file.c_str());
+        assert(dump_input == dump_output);
+
+        // Delete the temporary xml output.
+        unlink(out_file.c_str());
     }
 }
 
