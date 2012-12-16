a43d7878c2b3dab098c5663d16ddc82dd18ba3dd
Kohei Yoshida <kohei.yoshida@gmail.com>
2012-12-16 17:46:47 -0500

    Use the official v1.2 Relax NG schema file.

--- misc/generate-tokens.sh
+++ misc/generate-tokens.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 ./bin/gen-odf-tokens.py \
-    misc/OpenDocument-schema-v1.2-cd04.rng \
-    include/orcus/odf/odf_token_constants.inl \
+    misc/OpenDocument-v1.2-os-schema.rng \
+    src/liborcus/odf_token_constants.inl \
     src/liborcus/odf_tokens.inl \
     src/liborcus/odf_tokens.txt
 
 ./bin/gen-ooxml-tokens.py -t ooxml \
     misc/ooxml-ecma-376/OfficeOpenXML-XMLSchema.zip \
-    include/orcus/ooxml/ooxml_token_constants.inl \
+    src/liborcus/ooxml_token_constants.inl \
     src/liborcus/ooxml_tokens.inl \
     src/liborcus/ooxml_tokens.txt
 
 ./bin/gen-ooxml-tokens.py -t opc \
     misc/ooxml-ecma-376/OpenPackagingConventions-XMLSchema.zip \
-    include/orcus/ooxml/opc_token_constants.inl \
+    src/liborcus/opc_token_constants.inl \
     src/liborcus/ooxml_opc_tokens.inl \
     src/liborcus/ooxml_opc_tokens.txt
