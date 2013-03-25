58d3b85cc896fa71fa4e5f5887803c2ef2f5ff44
Kohei Yoshida <kohei.yoshida@gmail.com>
2013-03-24 20:27:30 -0400

    Hide global constant values.

--- src/parser/types.cpp
+++ src/parser/types.cpp
@@ -1,6 +1,6 @@
 /*************************************************************************
  *
- * Copyright (c) 2010-2012 Kohei Yoshida
+ * Copyright (c) 2013 Kohei Yoshida
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -25,45 +25,11 @@
  *
  ************************************************************************/
 
-#ifndef __ORCUS_TYPES_HPP__
-#define __ORCUS_TYPES_HPP__
-
-#include <cstdlib>
-#include <vector>
-#include <string>
-#include "orcus/pstring.hpp"
+#include "orcus/types.hpp"
 
 namespace orcus {
 
-typedef size_t xml_token_t;
-typedef const char* xmlns_id_t;
-
-ORCUS_DLLPUBLIC extern const xmlns_id_t XMLNS_UNKNOWN_ID;
-ORCUS_DLLPUBLIC extern const xml_token_t XML_UNKNOWN_TOKEN;
-
-struct xml_name_t
-{
-    xmlns_id_t ns;
-    pstring name;
-
-    xml_name_t() : ns(XMLNS_UNKNOWN_ID), name() {}
-    xml_name_t(xmlns_id_t _ns, const pstring& _name) : ns(_ns), name(_name) {}
-    xml_name_t(const xml_name_t& r) : ns(r.ns), name(r.name) {}
-};
-
-struct xml_token_attr_t
-{
-    xmlns_id_t ns;
-    xml_token_t name;
-    pstring value;
-
-    xml_token_attr_t() : ns(XMLNS_UNKNOWN_ID), name(XML_UNKNOWN_TOKEN) {}
-    xml_token_attr_t(xmlns_id_t _ns, xml_token_t _name, const pstring& _value) :
-        ns(_ns), name(_name), value(_value) {}
-};
-
-typedef ::std::vector<xml_token_attr_t> xml_attrs_t;
+const xmlns_id_t XMLNS_UNKNOWN_ID = NULL;
+const xml_token_t XML_UNKNOWN_TOKEN = 0;
 
 }
-
-#endif
