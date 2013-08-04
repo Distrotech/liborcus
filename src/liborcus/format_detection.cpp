e723a36a778c6e090dc020c11f0d430bed067ed9
Kohei Yoshida <kohei.yoshida@gmail.com>
2013-08-03 22:33:22 -0400

    Start working on format detection code.
    It's just a skeleton for now.

---
+++ src/liborcus/format_detection.cpp
@@ -0,0 +1,48 @@
+/*************************************************************************
+ *
+ * Copyright (c) 2013 Kohei Yoshida
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ ************************************************************************/
+
+#include "orcus/format_detection.hpp"
+
+#include <iostream>
+
+#ifdef __ORCUS_ODS
+#define ODS_ENABLED 1
+#else
+#define ODS_ENABLED 0
+#endif
+
+using namespace std;
+
+namespace orcus {
+
+format_t detect(const unsigned char* buffer, size_t length)
+{
+    cout << "ods enabled: " << ODS_ENABLED << endl;
+    return format_unknown;
+}
+
+}
