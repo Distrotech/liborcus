ee2b9a629ac0a6a7f4535563ba78c1bfca933ea8
Kohei Yoshida <kohei.yoshida@gmail.com>
2013-09-21 18:40:32 -0400

    Check the output directory presence.

--- src/orcus_ods_main.cpp
+++ src/orcus_ods_main.cpp
@@ -32,6 +32,7 @@
 
 #include <boost/scoped_ptr.hpp>
 #include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
 #include <vector>
 
 namespace {
@@ -55,6 +56,7 @@ using namespace orcus;
 int main(int argc, char** argv)
 {
     namespace po = boost::program_options;
+    namespace fs = boost::filesystem;
 
     po::options_description desc("Allowed options");
     desc.add_options()
@@ -121,6 +123,17 @@ int main(int argc, char** argv)
         cerr << "No output format specified.  Choose either 'flat' or 'html'." << endl;
         return EXIT_FAILURE;
     }
+
+    if (fs::exists(outdir))
+    {
+        if (!fs::is_directory(outdir))
+        {
+            cerr << "A file named '" << outdir << "' already exists, and is not a directory." << endl;
+            return EXIT_FAILURE;
+        }
+    }
+    else
+        fs::create_directory(outdir);
 
     boost::scoped_ptr<spreadsheet::document> doc(new spreadsheet::document);
     boost::scoped_ptr<spreadsheet::import_factory> fact(new spreadsheet::import_factory(doc.get()));
