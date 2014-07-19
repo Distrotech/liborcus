f97f067d637d78c19e8fd1928038d2a8c8759944
Kohei Yoshida <kohei.yoshida@gmail.com>
2014-07-19 12:46:49 -0400

    Add documentation of the config options.

--- include/orcus/config.hpp
+++ include/orcus/config.hpp
@@ -14,7 +14,16 @@ namespace orcus {
 
 struct ORCUS_DLLPUBLIC config
 {
+    /**
+     * Enable or disable runtime debug output to stdout or stderr.
+     */
     bool debug;
+
+    /**
+     * Control whether or not to perform strict check of the xml structure of
+     * a stream being parsed.  When enabled, it throws an xml_structure_error
+     * exception when an incorrect xml structure is detected.
+     */
     bool structure_check;
 
     config();
