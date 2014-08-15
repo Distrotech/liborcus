24c014b962f3944e8804771e838436f933ed6671
Markus Mohrhard <markus.mohrhard@googlemail.com>
2014-08-15 23:07:25 +0200

    allow standalone table import from xlsx
    This allows to use the table import for small parts of an import process
    without the need to bring in the whole orcus in one big step.

---
+++ include/orcus/orcus_import_xlsx.hpp
@@ -0,0 +1,33 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef ORCUS_ORCUS_XLSX_HPP
+#define ORCUS_ORCUS_XLSX_HPP
+
+#include "interface.hpp"
+
+namespace orcus {
+
+namespace spreadsheet { namespace iface {
+    class import_table;
+}}
+
+class ORCUS_DLLPUBLIC import_xlsx
+{
+private:
+    import_xlsx(); // deleted
+    import_xlsx(const import_xlsx&); // deleted
+    import_xlsx& operator=(const import_xlsx&); // deleted
+
+public:
+    static void read_table(const char* p, size_t n, spreadsheet::iface::import_table* data);
+};
+
+}
+
+#endif
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ src/liborcus/orcus_import_xlsx.cpp
@@ -0,0 +1,55 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "orcus/orcus_import_xlsx.hpp"
+
+#include "orcus/xml_namespace.hpp"
+#include "orcus/global.hpp"
+#include "orcus/spreadsheet/import_interface.hpp"
+#include "orcus/config.hpp"
+
+#include "xlsx_types.hpp"
+#include "xlsx_handler.hpp"
+#include "xlsx_context.hpp"
+#include "ooxml_tokens.hpp"
+
+#include "xml_stream_parser.hpp"
+#include "ooxml_namespace_types.hpp"
+#include "xlsx_session_data.hpp"
+#include "ooxml_global.hpp"
+
+#include <boost/scoped_ptr.hpp>
+
+namespace orcus {
+
+void import_xlsx::read_table(const char* p, size_t n, spreadsheet::iface::import_table* table)
+{
+    if(!table)
+        return;
+
+    if (!p || !n)
+        return;
+
+    session_context cxt;
+    boost::scoped_ptr<xlsx_table_xml_handler> handler(
+        new xlsx_table_xml_handler(cxt, ooxml_tokens, *table));
+
+    xmlns_repository ns_repo;
+    ns_repo.add_predefined_values(NS_ooxml_all);
+    ns_repo.add_predefined_values(NS_opc_all);
+    ns_repo.add_predefined_values(NS_misc_all);
+
+    xml_stream_parser parser(
+        orcus::config(), ns_repo, ooxml_tokens,
+        p, n);
+    parser.set_handler(handler.get());
+    parser.parse();
+}
+
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
