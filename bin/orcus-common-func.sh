10aab2b2085ec6a4b07c88bde3815e743aa6da8a
Kohei Yoshida <kohei.yoshida@gmail.com>
2015-03-03 21:14:12 -0500

    Add script to execute test binary independently.
    This will makes it easier to debug in case a test fails.

--- bin/orcus-common-func.sh
+++ bin/orcus-common-func.sh
@@ -1,16 +1,6 @@
 
-orcus_exec()
+common_exec()
 {
-    EXEC="$1"
-    shift
-    EXECDIR="$1"
-    shift
-    PROGDIR="$PWD/"`dirname $0`
-    ROOTDIR=$PROGDIR/..
-    EXECPATH=$ROOTDIR/src/.libs/$EXEC
-    export LD_LIBRARY_PATH=$ROOTDIR/src/liborcus/.libs:$ROOTDIR/src/mso/.libs:$ROOTDIR/src/parser/.libs:$ROOTDIR/src/spreadsheet/.libs:
-    cd $EXECDIR
-
     ARG_ONE=$1
     if [ "$ARG_ONE" == "gdb" ]; then
         shift
@@ -36,4 +26,33 @@ orcus_exec()
     # normal execution
     exec $EXECPATH "$@" || exit 1
     exit 0
+}
+
+orcus_exec()
+{
+    EXEC="$1"
+    shift
+    EXECDIR="$1"
+    shift
+    PROGDIR="$PWD/"`dirname $0`
+    ROOTDIR=$PROGDIR/..
+    EXECPATH=$ROOTDIR/src/.libs/$EXEC
+    export LD_LIBRARY_PATH=$ROOTDIR/src/liborcus/.libs:$ROOTDIR/src/mso/.libs:$ROOTDIR/src/parser/.libs:$ROOTDIR/src/spreadsheet/.libs:
+    cd $EXECDIR
+
+    common_exec "$@"
+}
+
+liborcus_test_exec()
+{
+    EXEC="$1"
+    shift
+    EXECDIR="$PROGDIR/../src/liborcus"
+    PROGDIR="$PWD/"`dirname $0`
+    ROOTDIR=$PROGDIR/..
+    EXECPATH=$ROOTDIR/src/liborcus/.libs/$EXEC
+    export LD_LIBRARY_PATH=$ROOTDIR/src/liborcus/.libs:$ROOTDIR/src/mso/.libs:$ROOTDIR/src/parser/.libs:$ROOTDIR/src/spreadsheet/.libs:
+    cd $EXECDIR
+
+    common_exec "$@"
 }
