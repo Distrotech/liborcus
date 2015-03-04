3cd5c3a12d10bd0a03053f64b5999b1d0137596d
Kohei Yoshida <kohei.yoshida@gmail.com>
2015-03-03 21:58:32 -0500

    Check property values in unit test.

--- src/liborcus/css_document_tree_test.cpp
+++ src/liborcus/css_document_tree_test.cpp
@@ -11,9 +11,29 @@
 
 #include <cstdlib>
 #include <cassert>
+#include <iostream>
 
 using namespace orcus;
 using namespace std;
+
+bool check_prop(const css_properties_t& props, const pstring& key, const pstring& val)
+{
+    css_properties_t::const_iterator it = props.find(key);
+    if (it == props.end())
+    {
+        cout << "property '" << key << "' not found" << endl;
+        return false;
+    }
+
+    if (it->second != val)
+    {
+        cout << "property '" << key << "' is expected to have value '"
+            << val << "' but '" << it->second << "' is found." << endl;
+        return false;
+    }
+
+    return true;
+}
 
 void test_css_parse_basic1()
 {
@@ -28,11 +48,16 @@ void test_css_parse_basic1()
 
     const css_properties_t* props = doc.get_properties(selector);
     assert(props);
+    assert(check_prop(*props, "width", "auto"));
+    assert(check_prop(*props, "height", "500px"));
 
     selector.first.name = "td";
     props = doc.get_properties(selector);
     assert(props);
+    assert(check_prop(*props, "color", "gray"));
+    assert(check_prop(*props, "background-color", "yellow"));
 
+    // This selector doesn't exist in the document tree.
     selector.first.name = "tr";
     props = doc.get_properties(selector);
     assert(!props);
