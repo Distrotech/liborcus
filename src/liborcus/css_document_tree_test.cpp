aff0a2cda3792cb4eef2dfc0f8af158dcb2648dd
Kohei Yoshida <kohei.yoshida@gmail.com>
2015-03-09 20:42:19 -0400

    Support selector with multiple classes.

--- src/liborcus/css_document_tree_test.cpp
+++ src/liborcus/css_document_tree_test.cpp
@@ -49,6 +49,28 @@ bool check_prop(const css_properties_t& props, const pstring& key, const pstring
     return true;
 }
 
+void test_css_simple_selector_equality()
+{
+    css_simple_selector_t left;
+    css_simple_selector_t right;
+    assert(left == right);
+
+    left.classes.insert("one");
+    assert(left != right);
+    right.classes.insert("one");
+    assert(left == right);
+
+    left.classes.insert("two");
+    assert(left != right);
+    right.classes.insert("two");
+    assert(left == right);
+
+    left.classes.insert("three");
+    assert(left != right);
+    right.classes.insert("three");
+    assert(left == right);
+}
+
 void test_css_parse_basic1()
 {
     const char* path = SRCDIR"/test/css/basic1.css";
@@ -227,14 +249,69 @@ void test_css_parse_basic6()
     assert(check_prop(*props, "letter-spacing", "0.5em"));
 }
 
+void test_css_parse_basic7()
+{
+    const char* path = SRCDIR"/test/css/basic7.css";
+    string strm;
+    load_file_content(path, strm);
+    css_document_tree doc;
+    doc.load(strm);
+
+    css_selector_t selector;
+    selector.first.classes.insert("one");
+    selector.first.classes.insert("two");
+    selector.first.classes.insert("three");
+
+    const css_properties_t* props = doc.get_properties(selector);
+    assert(props);
+    assert(props->size() == 1);
+    assert(check_prop(*props, "color", "blue"));
+
+    selector.first.classes.clear();
+    selector.first.classes.insert("one");
+    props = doc.get_properties(selector);
+    assert(props);
+    assert(props->size() == 1);
+    assert(check_prop(*props, "color", "aqua"));
+
+    selector.first.classes.clear();
+    selector.first.classes.insert("two");
+    props = doc.get_properties(selector);
+    assert(props);
+    assert(props->size() == 1);
+    assert(check_prop(*props, "color", "azure"));
+
+    selector.first.classes.insert("one"); // one two
+    props = doc.get_properties(selector);
+    assert(props);
+    assert(props->size() == 1);
+    assert(check_prop(*props, "color", "brown"));
+
+    selector.first.clear();
+    selector.first.name = "span";
+    selector.first.classes.insert("button");
+    props = doc.get_properties(selector);
+    assert(props);
+    assert(props->size() == 1);
+    assert(check_prop(*props, "border", "solid 1px gray"));
+
+    selector.first.classes.insert("selected"); // button selected
+    props = doc.get_properties(selector);
+    assert(props);
+    assert(props->size() == 1);
+    assert(check_prop(*props, "border", "solid 4px red"));
+}
+
 int main()
 {
+    test_css_simple_selector_equality();
     test_css_parse_basic1();
     test_css_parse_basic2();
     test_css_parse_basic3();
     test_css_parse_basic4();
     test_css_parse_basic5();
     test_css_parse_basic6();
+    test_css_parse_basic7();
 
     return EXIT_SUCCESS;
 }
