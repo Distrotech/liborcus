e9517a147cb7bf9b768561c47175d4797f1dd0e3
Kohei Yoshida <kohei.yoshida@gmail.com>
2015-04-04 17:27:21 -0400

    Add a csv_parser handler skeleton too (and a test for it).

--- parser_handlers/csv_test.cpp
+++ parser_handlers/csv_test.cpp
@@ -5,19 +5,21 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include "css.hpp"
-#include <orcus/css_parser.hpp>
+#include "csv.hpp"
+#include <orcus/csv_parser.hpp>
 
 #include <cstring>
 
 int main()
 {
-    const char* test_code = "p { background-color: white; }";
+    const char* test_code = "1,2,3,4,5\n6,7,8,9,10\n";
     size_t n = strlen(test_code);
 
-    orcus::css_parser_handler hdl;
-    orcus::css_parser<orcus::css_parser_handler> parser(test_code, n, hdl);
+    orcus::csv_parser_handler hdl;
+    orcus::csv::parser_config config;
+    orcus::csv_parser<orcus::csv_parser_handler> parser(test_code, n, hdl, config);
     parser.parse();
+
     return 0;
 }
 
