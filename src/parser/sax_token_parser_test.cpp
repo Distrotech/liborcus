847501f3d3ec54538a2252ab92e3f747f72314fa
Kohei Yoshida <kohei.yoshida@gmail.com>
2015-04-09 22:44:54 -0400

    Set up test framework for sax_token_parser.

---
+++ src/parser/sax_token_parser_test.cpp
@@ -0,0 +1,22 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "orcus/sax_token_parser.hpp"
+#include "orcus/tokens.hpp"
+
+void test_sax_token_parser_raw_string_name()
+{
+    // TODO : write test.
+}
+
+int main()
+{
+    test_sax_token_parser_raw_string_name();
+    return EXIT_SUCCESS;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
