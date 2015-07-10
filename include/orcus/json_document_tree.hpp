63d306f2b761a9bb7f7f26a5efaf69a89dc98f85
Kohei Yoshida <kohei.yoshida@gmail.com>
2015-07-10 18:58:27 -0400

    Add skeleton document tree storage for JSON documents.
    And an empty test file for it.

---
+++ include/orcus/json_document_tree.hpp
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_ORCUS_JSON_DOCUMENT_TREE_HPP
+#define INCLUDED_ORCUS_JSON_DOCUMENT_TREE_HPP
+
+#include "orcus/env.hpp"
+
+#include <string>
+
+namespace orcus {
+
+class ORCUS_DLLPUBLIC json_document_tree
+{
+public:
+    json_document_tree();
+    ~json_document_tree();
+
+    void load(const std::string& strm);
+};
+
+}
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ src/liborcus/json_document_tree.cpp
@@ -0,0 +1,21 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "orcus/json_document_tree.hpp"
+
+namespace orcus {
+
+json_document_tree::json_document_tree() {}
+json_document_tree::~json_document_tree() {}
+
+void json_document_tree::load(const std::string& strm)
+{
+}
+
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
