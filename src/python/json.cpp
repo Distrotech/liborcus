82e67daf7d111a83c1f284346409adfeb4bf93ac
Kohei Yoshida <kohei.yoshida@gmail.com>
2015-09-30 21:49:10 -0400

    python: implement orcus.json.loads() using json_document_tree.
    Intentially similar to the built-in json.loads().

--- src/python/json.cpp
+++ src/python/json.cpp
@@ -6,8 +6,12 @@
  */
 
 #include "orcus/env.hpp"
+#include "orcus/json_document_tree.hpp"
+#include "orcus/config.hpp"
+#include "orcus/pstring.hpp"
 
 #include <iostream>
+#include <algorithm>
 
 #include <Python.h>
 
@@ -36,17 +40,86 @@ int orcus_clear(PyObject* m)
     return 0;
 }
 
-PyObject* test(PyObject*, PyObject*)
+PyObject* to_pyobject(const json_document_tree::node& node)
 {
-    cout << "test" << endl;
+    switch (node.type())
+    {
+        case json_node_t::object:
+        {
+            vector<pstring> keys = node.keys();
+            PyObject* o = PyDict_New();
 
-    Py_INCREF(Py_None);
-    return Py_None;
+            std::for_each(keys.begin(), keys.end(),
+                [&](const pstring& key)
+                {
+                    PyObject* k = PyUnicode_FromStringAndSize(key.get(), key.size());
+                    PyObject* v = to_pyobject(node.child(key));
+                    PyDict_SetItem(o, k, v);
+                }
+            );
+            return o;
+        }
+        case json_node_t::array:
+        {
+            size_t n = node.child_count();
+            PyObject* o = PyList_New(node.child_count());
+            for (size_t i = 0; i < n; ++i)
+            {
+                json_document_tree::node child = node.child(i);
+                PyObject* item = to_pyobject(child);
+                if (!item)
+                    return nullptr;
+
+                PyList_SetItem(o, i, item);
+            }
+            return o;
+        }
+        case json_node_t::number:
+            return PyFloat_FromDouble(node.numeric_value());
+        case json_node_t::string:
+        {
+            pstring sv = node.string_value();
+            return PyUnicode_FromStringAndSize(sv.get(), sv.size());
+        }
+        case json_node_t::boolean_true:
+            Py_INCREF(Py_True);
+            return Py_True;
+        case json_node_t::boolean_false:
+            Py_INCREF(Py_False);
+            return Py_False;
+        case json_node_t::null:
+            Py_INCREF(Py_None);
+            return Py_None;
+        default:
+            ;
+    }
+
+    PyErr_SetString(PyExc_TypeError, "Unexpected JSON node type.");
+    return nullptr;
+}
+
+PyObject* json_loads(PyObject* /*module*/, PyObject* args, PyObject* kwargs)
+{
+    char* stream = NULL;
+    static const char* kwlist[] = { "s", NULL };
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s", const_cast<char**>(kwlist), &stream))
+    {
+        PyErr_SetString(PyExc_TypeError, "The method must be given a string.");
+        return nullptr;
+    }
+
+    cout << stream << endl;
+
+    json_config conf;
+    json_document_tree doc;
+    doc.load(stream, conf);
+
+    return to_pyobject(doc.get_document_root());
 }
 
 PyMethodDef orcus_methods[] =
 {
-    { "test", (PyCFunction)test, METH_NOARGS, "Test." },
+    { "loads", (PyCFunction)json_loads, METH_VARARGS | METH_KEYWORDS, "Load JSON string into a Python object." },
     { NULL, NULL, 0, NULL }
 };
 
