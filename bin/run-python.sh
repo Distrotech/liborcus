0070bc2e274ad3188a94cc1006c5f3ca8674b9c6
Kohei Yoshida <kohei.yoshida@gmail.com>
2016-03-25 20:20:52 -0400

    Add gdb mode to allow debugging with gdb.

--- bin/run-python.sh
+++ bin/run-python.sh
@@ -5,17 +5,33 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 
+RUNMODE=
+
+if [ "$1" == "gdb" ]; then
+    RUNMODE=gdb
+    shift
+fi
+
 if [ ! -e "$1" ]; then
     echo "file '$1' does not exist"
     exit 1
 fi
 
+PYTHON=$(which python3)
 PROGDIR=`dirname $0`
 _PYTHONPATH="$PROGDIR/../src/python/.libs:$PROGDIR/../src/python"
 
 export PYTHONPATH=$_PYTHONPATH:$PYTHONPATH
 export LD_LIBRARY_PATH="$PROGDIR/../src/liborcus/.libs:$PROGDIR/../src/parser/.libs"
 export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
-exec $PWD/"$1"
+
+case $RUNMODE in
+    gdb)
+    gdb --args $PYTHON $PWD/"$1"
+        ;;
+    *)
+        exec $PWD/"$1"
+        ;;
+esac
 
 
