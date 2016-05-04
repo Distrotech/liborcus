54a4b3c761a709989486d6405a9b21de604a9646
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-05-04 14:46:01 +0300

    add license header

--- src/liborcus/string_helper.cpp
+++ src/liborcus/string_helper.cpp
@@ -1,3 +1,10 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #include "string_helper.hpp"
 
 namespace orcus {
@@ -29,3 +36,5 @@ std::vector<pstring> string_helper::split_string(const pstring& str, const char 
 }
 
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
