ae00044b52255615f2d759c158d1d43379c21bb4
Kohei Yoshida <kohei.yoshida@gmail.com>
2016-06-07 22:52:33 -0400

    Dump cell values, except for formula cells.
    BTW, this change requires mdds 1.2.1 or above, for the handling of boolean
    element block.

--- src/spreadsheet/json_dumper.cpp
+++ src/spreadsheet/json_dumper.cpp
@@ -18,6 +18,53 @@
 
 namespace orcus { namespace spreadsheet { namespace detail {
 
+using columns_type = mdds::mtv::collection<ixion::column_store_t>;
+
+namespace {
+
+void dump_cell_value(
+    std::ofstream& file, const ixion::model_context& cxt,
+    const columns_type::const_iterator::value_type& node)
+{
+    switch (node.type)
+    {
+        case ixion::element_type_empty:
+            file << "null";
+            break;
+        case ixion::element_type_boolean:
+        {
+            auto b = node.get<ixion::boolean_element_block>();
+            file << (b ? "true" : "false");
+        }
+        break;
+        case ixion::element_type_numeric:
+        {
+            auto v = node.get<ixion::numeric_element_block>();
+            file << v;
+        }
+        break;
+        case ixion::element_type_string:
+        {
+            size_t sindex = node.get<ixion::string_element_block>();
+            const std::string* p = cxt.get_string(sindex);
+            assert(p);
+
+            // TODO : escape this string for json.
+            file << '"' << *p << '"';
+        }
+        break;
+        case ixion::element_type_formula:
+        {
+            // TODO : handle formula cells.
+        }
+        break;
+        default:
+            ;
+    }
+}
+
+}
+
 json_dumper::json_dumper(const document& doc) : m_doc(doc) {}
 
 void json_dumper::dump(const std::string& filepath, ixion::sheet_t sheet_id) const
@@ -37,7 +84,6 @@ void json_dumper::dump(const std::string& filepath, ixion::sheet_t sheet_id) con
     if (!p)
         return;
 
-    using columns_type = mdds::mtv::collection<ixion::column_store_t>;
     columns_type columns(p->begin(), p->end());
 
     // Only iterate through the data range.
@@ -60,7 +106,9 @@ void json_dumper::dump(const std::string& filepath, ixion::sheet_t sheet_id) con
     size_t col = it->index;
 
     file << "    {";
-    file << "\"" << column_labels[col] << "\": null";
+    file << "\"" << column_labels[col] << "\": ";
+
+    dump_cell_value(file, cxt, *it);
 
     size_t last_col = col;
     size_t last_row = row;
@@ -81,8 +129,7 @@ void json_dumper::dump(const std::string& filepath, ixion::sheet_t sheet_id) con
 
             file << "\"" << column_labels[col] << "\": ";
 
-            // TODO : Get the real cell value.
-            file << "null";
+            dump_cell_value(file, cxt, node);
 
             last_col = node.index;
             last_row = node.position;
