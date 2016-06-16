c17c185d7f0e194ce17a6f90cb1c2103a834778f
Jaskaran Singh <jvsg1303@gmail.com>
2016-06-16 17:45:07 +0530

    Add code for text style import

--- src/liborcus/odf_number_formatting_context.cpp
+++ src/liborcus/odf_number_formatting_context.cpp
@@ -713,6 +713,19 @@ void number_formatting_context::start_element(xmlns_id_t ns, xml_token_t name, c
                     m_current_style->number_formatting_code += "0";
             }
             break;
+            case XML_text_style:
+            {
+                text_style_attr_parser func;
+                func = std::for_each(attrs.begin(), attrs.end(), func);
+                m_current_style->name = func.get_style_name();
+                m_current_style->is_volatile = func.is_volatile();
+            }
+            break;
+            case XML_text_content:
+            {
+                m_current_style->number_formatting_code += "@";
+            }
+            break;
             default:
                 ;
         }
@@ -739,7 +752,8 @@ bool number_formatting_context::end_element(xmlns_id_t ns, xml_token_t name)
 {
     if (ns == NS_odf_number)
     {
-        if (name == XML_number_style || name == XML_currency_style || name == XML_percentage_style)
+        if (name == XML_number_style || name == XML_currency_style || name == XML_percentage_style
+            || name == XML_text_style)
         {
             if (m_current_style->is_volatile)
             {
